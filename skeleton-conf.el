43569d80293d1e14cd7433b804e286507615904f
newlong_10.04 <newlong@126.com>
2012-12-03 23:56:03 +0800

    注释单引号补全

--- skeleton-conf.el
+++ skeleton-conf.el
@@ -17,6 +17,6 @@
 ;; 也可以绑定单独到某个mode，比如cc-mode (define-key cc-mode-map (kbd "(") 'skeleton-pair-insert-maybe)
 (global-set-key (kbd "(")  'skeleton-pair-insert-maybe)
 (global-set-key (kbd "{")  'skeleton-pair-insert-maybe)
-(global-set-key (kbd "\'") 'skeleton-pair-insert-maybe)
+;(global-set-key (kbd "\'") 'skeleton-pair-insert-maybe)
 (global-set-key (kbd "\"") 'skeleton-pair-insert-maybe)
 (global-set-key (kbd "[")  'skeleton-pair-insert-maybe)
